69bd09f2178ba20f091477061f59933e6ebb0c0a
danzharuk1 <zharuk.dan@gmail.com>
2022-08-05 01:49:07 +0300

    re-main

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from random import *
 
+direction = ''
+dictionary = ''
+step = 0
+
 print('Вас приветствует "Шифр Цезаря"')
+print('Выберите направление(шифрование/дешифрование)')
 
-def answer(t, a1, b1):
-    print(t)
-    while True:
-        a = input().lower()
-        if a == a1:
-            return True
-        elif a == b1:
-            return False
+while True:
+    t = input().lower()
 
+    if t == 'шифрование':
+        direction = t
+        break
+
+    elif t == 'дешифрование':
+        direction = t
+        break
+
+print('Выберите язык алфавита(русский/английский)')
+
+while True:
+    t = input().lower()
+
+    if t == 'русский':
+       dictionary = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    elif t == 'английский':
+        dictionary = 'abcdefghijklmnopqrstuvwxyz '
+
+print('укажите шаг:')
+while True:
+    t = input()
+    for i in range(len(t)):
+        if t[i] not in '0123456789':
+            break
+    else:
+        step = int(t)
+        break
