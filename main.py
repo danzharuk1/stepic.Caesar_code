3ec14a008255750b01e3d90085ee2818283332b7
danzharuk1 <zharuk.dan@gmail.com>
2022-08-07 02:39:09 +0300

    russian and english language support(encryption).in the next commit will be added decryption on both language

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from random import *
-
 direction = ''
 dictionary = ''
+CHAR = '~!@#$%^&*()_-=+-/*"|{}[],. \'1234567890'
+TEXT = ''
+result = ''
 step = 0
 
 print('Вас приветствует "Шифр Цезаря"')
-print('Выберите направление(шифрование/дешифрование)')
 
+print('Выберите направление(шифрование/дешифрование)')
 while True:
     t = input().lower()
 
@@ -19,15 +20,15 @@ while True:
         break
 
 print('Выберите язык алфавита(русский/английский)')
-
 while True:
     t = input().lower()
 
     if t == 'русский':
-       dictionary = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
+        dictionary = t
+        break
     elif t == 'английский':
-        dictionary = 'abcdefghijklmnopqrstuvwxyz '
+        dictionary = t
+        break
 
 print('укажите шаг:')
 while True:
@@ -38,3 +39,40 @@ while True:
     else:
         step = int(t)
         break
+
+print('Введите ваш текст')
+TEXT = input()
+
+if direction == 'шифрование':
+    for i in range(len(TEXT)):
+        if TEXT[i] not in CHAR:
+            if dictionary == 'английский':
+                if TEXT[i].isupper():
+                    if ord(TEXT[i]) + step > 90:
+                        result += chr((ord(TEXT[i]) + step + 65) % 91)
+                    else:
+                        result += chr(ord(TEXT[i]) + step)
+                else:
+                    if ord(TEXT[i]) + step > 122:
+                        result += chr((ord(TEXT[i]) + step + 97) % 123)
+                    else:
+                        result += chr(ord(TEXT[i]) + step)
+            else:
+                if TEXT[i].isupper():
+                    if ord(TEXT[i]) + step > 1071:
+                        result += chr((ord(TEXT[i]) + step + 1040) % 1072)
+                    else:
+                        result += chr(ord(TEXT[i]) + step)
+
+                else:
+                    if ord(TEXT[i]) + step > 1103:
+                        result += chr((ord(TEXT[i]) + step + 1072) % 1104)
+                    else:
+                        result += chr(ord(TEXT[i]) + step)
+
+        else:
+            result += TEXT[i]
+
+else:
+    pass
+print(result)
